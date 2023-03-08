dcf22d9c2d6e21d5f81063637b9347cb3c769391
badblues <borovikxv.a@gmail.com>
2023-03-08 12:15:26 +0700

    -

--- backend/domain/Task.cs
+++ backend/domain/Task.cs
@@ -1,5 +1,15 @@
 ﻿namespace domain;
 
-public class Task {
+using System;
+
+public class Task
+{
+  public Guid Id { get; set; }
+  public Guid UserId { get; set; }
+  public bool Completed { get; set; }
+  public String Title { get; set; }
+  public String Details { get; set; }
+  public DateTime CreationDate { get; set; }
+  public DateTime EditDate { get; set; }
 
 }
---
+++ backend/repository/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace repository;
+
+public interface IRepository<T> where T : Task
+{
+  T GetById(int id);
+  IEnumerable<T> List();
+  IEnumerable<T> List(Expression<Func<T, bool>> predicate);
+  void Add(T entity);
+  void Delete(T entity);
+  void Edit(T entity);
+}
