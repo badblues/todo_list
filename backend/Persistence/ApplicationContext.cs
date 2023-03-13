d667aa1d71a7805cad7d47ed036524b588dd5098
badbadblues _ <borovikxv.a@gmail.com>
2023-03-13 10:03:37 +0700

    added EF

---
+++ backend/Persistence/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain;
+using Microsoft.Data.SqlClient;
+
+namespace Persistence
+{
+    public class ApplicationContext : DbContext
+    {
+        private DbSet<TodoTask> Users { set; get; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString;
+            connectionString = @"Data Source=;Initial Catalog=TODODB;User ID=; Password=; TrustServerCertificate=True";
+            SqlConnection sqlConnection = new(connectionString);
+            sqlConnection.Open();
+            optionsBuilder.UseSqlServer(sqlConnection);
+        }
+    }
+}
