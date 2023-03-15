e6902239f1be16c5807745a697e5c3a861471099
badbadblues _ <borovikxv.a@gmail.com>
2023-03-15 22:56:48 +0700

    added config file

--- backend/Persistence/ApplicationContext.cs
+++ backend/Persistence/ApplicationContext.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Domain;
 using Microsoft.Data.SqlClient;
+using System.Configuration;
+using System.Collections.Specialized;
 
 namespace Persistence
 {
@@ -15,7 +17,9 @@ namespace Persistence
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string connectionString;
-            connectionString = @"Data Source=;Initial Catalog=TODODB;User ID=; Password=; TrustServerCertificate=True";
+            string? username = ConfigurationManager.AppSettings.Get("db_username");
+            string? password = ConfigurationManager.AppSettings.Get("db_password");
+            connectionString = @$"Data Source=;Initial Catalog=TODODB;User ID={username}; Password={password}; TrustServerCertificate=True";
             SqlConnection sqlConnection = new(connectionString);
             sqlConnection.Open();
             Console.WriteLine("CONNECTED TO MSQL SERVER.");
