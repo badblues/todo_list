b3a96523cb9afd75b4cc596792b164e10159e6c2
badbadblues _ <borovikxv.a@gmail.com>
2023-04-15 09:16:06 +0700

    fixed delete request

--- frontend/src/app/components/task-item/task-item.component.ts
+++ frontend/src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from 'src/app/Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,6 +8,13 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./task-item.component.css']
 })
 export class TaskItemComponent {
+
   @Input() task!: Task;
   faTimes = faTimes;
+
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task);
+  }
 }
