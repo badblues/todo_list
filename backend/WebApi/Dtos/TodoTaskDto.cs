478091e83ea7fe3cafc41c7fb0acb8490cdcd538
badblues <borovikxv.a@gmail.com>
2023-06-16 07:24:33 +0700

    ikd

--- backend/WebApi/Dtos/TodoTaskDto.cs
+++ backend/WebApi/Dtos/TodoTaskDto.cs
@@ -6,7 +6,7 @@
         public Guid UserId { get; init; }
         public bool Completed { get; set; }
         public string Title { get; set; } = String.Empty;
-        public string Details { get; set; } = String.Empty
+        public string Details { get; set; } = String.Empty;
         public DateTime CreationDate { get; set; }
         public DateTime? EditDate { get; set; }
     }
